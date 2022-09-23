ddd441aa4ecc4bd2620f1ada27c944d165c2513e
Dzmitry Viartseika <dviartseika@exadel.com>
2022-09-23 19:26:58 +0200

    added lambda function

---
+++ product-service/src/functions/getProduct/handler.ts
@@ -0,0 +1,81 @@
+// import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
+import { formatJSONResponse } from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
+
+// import schema from "./schema";
+
+const products = [
+    {
+        count: 4,
+        description: "Short Product Description1",
+        id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+        price: 2.4,
+        title: "ProductOne",
+    },
+    {
+        count: 6,
+        description: "Short Product Description3",
+        id: "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
+        price: 10,
+        title: "ProductNew",
+    },
+    {
+        count: 7,
+        description: "Short Product Description2",
+        id: "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
+        price: 23,
+        title: "ProductTop",
+    },
+    {
+        count: 12,
+        description: "Short Product Description7",
+        id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+        price: 15,
+        title: "ProductTitle",
+    },
+    {
+        count: 7,
+        description: "Short Product Description2",
+        id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+        price: 23,
+        title: "Product",
+    },
+    {
+        count: 8,
+        description: "Short Product Description4",
+        id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+        price: 15,
+        title: "ProductTest",
+    },
+    {
+        count: 2,
+        description: "Short Product Descriptio1",
+        id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+        price: 23,
+        title: "Product2",
+    },
+    {
+        count: 3,
+        description: "Short Product Description7",
+        id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+        price: 15,
+        title: "ProductName",
+    },
+];
+
+const getProduct = async (event) => {
+    const { id } = event.pathParameters;
+    const product = products.find((product) => product.id === id);
+
+    if (product) {
+        return formatJSONResponse({
+            data: [product],
+        });
+    }
+
+    return formatJSONResponse({
+        message: 'Error: Product not found!',
+    });
+};
+
+export const main = middyfy(getProduct);
